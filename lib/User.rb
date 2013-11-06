c865ca0930d6a8e3d1edf411ebbff676d45f669d
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-06 16:17:25 +0100

    Users with common favorite movies have factor 0

--- lib/User.rb
+++ lib/User.rb
@@ -6,17 +6,35 @@ class User
         @comments = []
         @favorite_movies = []
     end
+
     def new_comment title, comment
-        user_comment = {}
-        user_comment["Title"] = title
-        user_comment["Comment"] = comment
+        user_comment = {
+            title: title,
+            comment: comment
+
+        }
+        
         @comments << user_comment
     end
+
     def get_comments 
         @comments
     end
 
     def favorite_a_movie movie_title
-        @favorite_movies << movie_title
+        unless @favorite_movies.include? movie_title
+            @favorite_movies << movie_title
+        end
     end
+
+    def calculate_proximity user
+        factor = 1
+        @favorite_movies.each do |movie|
+            if user.favorite_movies.include? movie                
+                factor = 0
+            end
+        end
+        factor
+    end
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,10 +3,9 @@ require_relative '../lib/User.rb'
 
 user = User.new
 comments = [{
-        "Title" => "Spartacus",
-        "Comment" => "Awesome movie!"
+        :title => "Spartacus",
+        :comment => "Awesome movie!"
     }]
-
 
 describe "When a user comments" do
   
@@ -17,14 +16,27 @@ describe "When a user comments" do
 
   it "contains all the comments info" do
     user.new_comment "Blade Runner", "My favorite movie"
-    comments << { "Title" => "Blade Runner", "Comment" => "My favorite movie"}
+    comments << { :title => "Blade Runner", :comment => "My favorite movie"}
     expect(user.get_comments).to eq comments
   end
 end
 
 describe "When a user favorites a movie" do
-  it "the movie is favorited for that user" do
+  it "the movie is included in the favorites list" do
+    user.favorite_a_movie("Spartacus")
+    expect(user.favorite_movies).to include "Spartacus"
+  end
+
+  it "doesn't adds the movie if it is already favorited" do
     user.favorite_a_movie("Spartacus")
     expect(user.favorite_movies).to eq ["Spartacus"]
+  end
+end
+
+describe "When two users have the same favorite" do
+  it "they have proximity factor 0" do
+    user_prox_zero = User.new
+    user_prox_zero.favorite_a_movie("Spartacus")
+    expect(user.calculate_proximity(user)).to eq 0
   end
 end
